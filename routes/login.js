9f336a0d5378586fdccba248151fcafc001fc06d
hoangtin1514 <1312600@student.hcmus.edu.vn>
2016-06-23 18:23:49 +0700

    Login using facebook account

--- routes/login.js
+++ routes/login.js
@@ -15,5 +15,15 @@ router.post('/login', passport.authenticate('local', {
     failureRedirect: '/login'
 }));
 
+router.get('/auth/facebook',
+  passport.authenticate('facebook',{ scope: ['email']}
+));
 
+router.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      console.log("Login facebook thanh cong")
+      res.redirect('/');
+    });
 module.exports = router;
