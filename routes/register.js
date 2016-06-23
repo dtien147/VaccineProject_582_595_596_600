7850d9adfd3d51831938026f6be8362069cef024
Duong Tien <dtien147@gmail.com>
2016-06-23 14:07:38 +0700

    Check reg info on server

--- routes/register.js
+++ routes/register.js
@@ -6,9 +6,10 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
-    {
-        var db = req.db;
-        var users = db.get('users');
+    var db = req.db;
+    var users = db.get('users');
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(req.body.email)) {
         users.findOne({
             email: req.body.email
         }, function(err, result) {
@@ -16,18 +17,21 @@ router.post('/register', function(req, res) {
                 console.log(err);
             } else if (result != null) {
                 res.send(false);
-            } else {
+                console.log("User exists");
+                console.log(result);
+            } else if (req.body.name != '' && req.body.password != '') {
                 users.insert({
                     'email': req.body.email,
-                    'first_name': req.body.first_name,
-                    'last_name': req.body.last_name,
+                    'name': req.body.name,
                     'password': req.body.password
                 });
                 res.send('/');
+                console.log("Success");
             }
         });
-
-
+    } else {
+        res.send(false);
+        console.log("Email is incorrect");
     }
 });
 
